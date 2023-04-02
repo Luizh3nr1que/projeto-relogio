84509cf5c41a4ecfd71260af8b2e0d1817ff1b0e
luizh3nrique <luizh3nrique00@gmail.com>
2023-04-02 13:53:01 -0300

    Fix - adcionei data

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ let digital = document.querySelector('.digital');
 let sElement = document.querySelector('.p_s');
 let mElement = document.querySelector('.p_m');
 let hElement = document.querySelector('.p_h');
+let date = document.querySelector('.date');
 
 function updateClock() {
     let now = new Date();
     let hour = now.getHours();
     let minute = now.getMinutes();
     let second = now.getSeconds();
+    let dia = now.getDate().toString().padStart(2, '0');
+    let mes = (now.getMonth() + 1).toString().padStart(2, '0');
+    let ano = now.getFullYear();
+    let dataCompleta = dia + '/' + mes + '/' + ano;
 
 
     digital.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
@@ -19,6 +24,8 @@ function updateClock() {
     sElement.style.transform = `rotate(${sDeg}deg)`
     mElement.style.transform = `rotate(${mDeg}deg)`
     mElement.style.transform = `rotate(${hDeg}deg)`
+
+    date.innerHTML = `${dataCompleta}`
 
 
 }
